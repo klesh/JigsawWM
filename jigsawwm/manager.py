eb1c319762b80e4af9291141c0699e33152e3230
Klesh Wong <klesh@qq.com>
2022-12-22 01:15:00 +0800

    feat: add gap support

--- jigsawwm/manager.py
+++ jigsawwm/manager.py
@@ -12,12 +12,14 @@ class WindowManager:
     new_window_as_master: bool
     layout_tilers: List[LayoutTiler]
     ignore_exe_names: Set[str]
+    gap: int
 
     def __init__(
         self,
         new_window_as_master: bool = True,
         layout_tilers: List[LayoutTiler] = None,
         ignore_exe_names: Set[str] = None,
+        gap: int = 2,
     ):
         self._state = {}
         self.new_window_as_master = new_window_as_master
@@ -28,6 +30,7 @@ class WindowManager:
         ]
         self.current_layout_tiler_index = 0
         self.ignore_exe_names = set(ignore_exe_names or [])
+        self.gap = gap
 
     def sync(self) -> bool:
         state: Dict[Monitor, Set[Window]] = {}
@@ -72,37 +75,50 @@ class WindowManager:
     def arrange(self, monitor: Monitor):
         # now, here is the tricky part, thanks to the chaotic Win32 API
         #
-        #   1. the `SetWindowPos` api accept RECT in (x,y,w,h) order
-        #   2. the `dwmapi` renders window with DropShadow called Extended Frame Bounds
-        #      which may or may not make the actual window smaller than we expected
-        #   3. even worst, formmer api works in Scaled-Pixel while the latter in
-        #      Physical-Pixel
-        #   4. the monitor RECt works in Scaled-Pixel mode as well
-        #
-        # Ideally, we want the all windows sharing the same visible sizing.
-        # So, we gonna utilizing the dwmapi to gather the extended frame bounds and
-        # convert it into Scaled-Pixel
+        #   1. the `SetWindowPos` api accept Scaled-Pixel (x,y,w,h), however,
+        #      most of windows would be rendered smaller than the given value.
+        #   2. ideally, we want windows to be rendered in exact size we specified.
+        #      the only way I found is to get the actual size by `dwmapi` and
+        #      compensate the difference. however `DwmGetWindowAttribute` works in
+        #      Physical-Pixel, it must be scaled
+
         scale_factor = monitor.get_scale_factor().value / 100
         wr = monitor.get_info().rcWork
         work_area = (wr.left, wr.top, wr.right, wr.bottom)
         windows = self._state[monitor]
         i = 0
-        for rect in self.current_layout_tiler(work_area, len(windows)):
+        for (left, top, right, bottom) in self.current_layout_tiler(
+            work_area, len(windows)
+        ):
             window = windows[i]
-            # calculate the margin to the Extended Frame Bounds
+            # add gap
+            if self.gap:
+                if left == wr.left:
+                    left += self.gap
+                if top == wr.top:
+                    top += self.gap
+                if right == wr.right:
+                    right -= self.gap
+                if bottom == wr.bottom:
+                    bottom -= self.gap
+            left += self.gap
+            top += self.gap
+            right -= self.gap
+            bottom -= self.gap
+            # compensation
             r = window.get_rect()
             b = window.get_extended_frame_bounds()
-            adjusted_rect = (
-                round(rect[0] + r.left - (b.left / scale_factor)),
-                round(rect[1] + r.top - (b.top / scale_factor)),
-                round(rect[2] + r.right - (b.right / scale_factor)),
-                round(rect[3] + r.bottom - (b.bottom / scale_factor)),
+            compensated_rect = (
+                round(left + r.left - (b.left / scale_factor)),
+                round(top + r.top - (b.top / scale_factor)),
+                round(right + r.right - (b.right / scale_factor)),
+                round(bottom + r.bottom - (b.bottom / scale_factor)),
             )
-            print(window.title, rect, adjusted_rect)
+            print(window.title, (left, top, right, bottom), compensated_rect)
 
             # now, here is the tricky part
             # the
-            window.set_rect(RECT(*adjusted_rect))
+            window.set_rect(RECT(*compensated_rect))
             i += 1
 
     @property
