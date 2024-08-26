121aa5cddb20de8a0e3ca43a7866de9efe2ad10f
Klesh Wong <klesh@qq.com>
2024-08-27 00:01:57 +0800

    feat: add workspaces to splash window

--- src/jigsawwm/ui/windows_splash.py
+++ src/jigsawwm/ui/windows_splash.py
@@ -5,9 +5,9 @@ from typing import List, Optional
 from ctypes import * # pylint: disable=wildcard-import,unused-wildcard-import
 from ctypes.wintypes import * # pylint: disable=wildcard-import,unused-wildcard-import
 
-from PySide6.QtCore import QByteArray, QPoint, Qt, Signal, Slot
+from PySide6.QtCore import QPoint, Qt, Signal, Slot
 from PySide6.QtGui import QImage
-from PySide6.QtWidgets import QLabel, QSizePolicy, QWidget
+from PySide6.QtWidgets import QLabel, QSizePolicy, QWidget,QHBoxLayout, QVBoxLayout
 
 from jigsawwm.w32.window import Window
 from jigsawwm.wm.monitor_state import MonitorState
@@ -21,9 +21,10 @@ logger = logging.getLogger(__name__)
 class WindowsSplash(Dialog):
     """The window list splash screen."""
 
-    show_splash = Signal(MonitorState, Window)
+    show_splash = Signal(MonitorState, int, Window)
     hide_splash = Signal()
-    worspace_widget: QLabel
+    monitor_state: QLabel
+    workspace_states: QWidget
     windows: List[Window]
     shellhook_msgid = 0
     created_windows = set()
@@ -33,42 +34,74 @@ class WindowsSplash(Dialog):
         self.windows = []
         self.show_splash.connect(self.show_windows_splash)
         self.hide_splash.connect(self.hide_windows_splash)
-
-        self.worspace_widget = QLabel(self.root)
-        self.worspace_widget.setObjectName("workspace")
-        self.worspace_widget.setSizePolicy(
+        # monitor
+        self.monitor_state = QLabel(self.root)
+        self.monitor_state.setObjectName("monitor")
+        self.monitor_state.setSizePolicy(
             QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed
         )
-        self.worspace_widget.setText("Workspace: ?")
-        self.worspace_widget.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.root_layout.insertWidget(0, self.worspace_widget)
-        logger.debug("WindowsSplash init")
+        self.monitor_state.setText("Monitor: ?")
+        self.monitor_state.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.root_layout.insertWidget(0, self.monitor_state)
+        # workspace
+        self.workspace_states = QWidget(self.root)
+        self.workspace_states.setLayout(QHBoxLayout())
+        self.workspace_states.setFixedHeight(100)
+        self.root_layout.insertWidget(1, self.workspace_states)
+        logger.info("WindowsSplash init")
 
     @Slot(MonitorState, Window)
-    def show_windows_splash(self, monitor_state: MonitorState, active_window: Optional[Window]=None):
+    def show_windows_splash(self, monitor_state: MonitorState, workspace_index: int, active_window: Optional[Window]=None):
         """Show the splash screen"""
-        logger.debug("WindowsSplash show")
-        self.worspace_widget.setText(f"Workspace: {monitor_state.workspace.name} (theme: {monitor_state.workspace.theme_name})")
-        h = 0
+        logger.info("WindowsSplash show")
+        # monitor
+        self.monitor_state.setText(f"Monitor: {monitor_state.monitor.name}")
+        # workspaces
+        self.deleteDirectChildren(self.workspace_states)
+        for i, workspace in enumerate(monitor_state.workspaces):
+            widget = QWidget()
+            widget.setObjectName("workspace")
+            widget.setLayout(QVBoxLayout())
+            widget.setProperty("active", i == workspace_index)
+            # name
+            ws_name = QLabel(workspace.name)
+            ws_name.setObjectName("workspace_name")
+            ws_name.setSizePolicy(
+                QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding
+            )
+            ws_name.setAlignment(Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignBottom)
+            widget.layout().addWidget(ws_name)
+            # theme
+            ws_info = QLabel(workspace.theme_name)
+            ws_info.setObjectName("workspace_info")
+            ws_info.setSizePolicy(
+                QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding
+            )
+            ws_info.setAlignment(Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignTop)
+            widget.layout().addWidget(ws_info)
+
+            self.workspace_states.layout().addWidget(widget)
+        h = self.workspace_states.height()
         w = self.width()
-        if self.windows != monitor_state.tilable_windows:
+        tilable_windows = monitor_state.workspaces[workspace_index].tilable_windows
+        if self.windows != tilable_windows:
             self.deleteDirectChildren(self.container)
-            self.windows = monitor_state.tilable_windows.copy()
+            self.windows = tilable_windows.copy()
             for window in self.windows:
                 icon = QImage.fromHICON(window.icon_handle)
-                widget: QWidget = self.create_row_widget(
+                ws_name: QWidget = self.create_row_widget(
                     self.container.width(), window.title, icon
                 )
-                widget.setObjectName("row")
-                widget.setProperty("handle", window.handle)
-                widget.setSizePolicy(
+                ws_name.setObjectName("row")
+                ws_name.setProperty("handle", window.handle)
+                ws_name.setSizePolicy(
                     QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed
                 )
-                self.container_layout.addWidget(widget)
-                widget.setFixedHeight(36)
+                self.container_layout.addWidget(ws_name)
+                ws_name.setFixedHeight(36)
                 h += 36
-                if w < widget.width():
-                    w = widget.width()
+                if w < ws_name.width():
+                    w = ws_name.width()
         if len(self.windows) == 0:
             self.deleteDirectChildren(self.container)
             self.spacer = QLabel("Nothing here")
@@ -79,12 +112,12 @@ class WindowsSplash(Dialog):
             self.container_layout.addWidget(self.spacer)
         if active_window:
             for i in range(self.container_layout.count()):
-                widget = self.container_layout.itemAt(i).widget()
-                if widget.property("handle") == active_window.handle:
-                    widget.setProperty("active", True)
+                ws_name = self.container_layout.itemAt(i).widget()
+                if ws_name.property("handle") == active_window.handle:
+                    ws_name.setProperty("active", True)
                 else:
-                    widget.setProperty("active", False)
-                widget.setStyleSheet("")
+                    ws_name.setProperty("active", False)
+                ws_name.setStyleSheet("")
         # centering the window
         r = monitor_state.monitor.get_rect()
         rect = app.screenAt(QPoint(r.left, r.top)).geometry()
@@ -96,26 +129,25 @@ class WindowsSplash(Dialog):
     @Slot()
     def hide_windows_splash(self):
         """Hide the splash screen"""
-        logger.debug("WindowsSplash hide")
+        logger.info("WindowsSplash hide")
         self.hide()
 
-    def nativeEvent(self, eventType: QByteArray | bytes, message: int) -> object:
-        if eventType == "windows_generic_MSG":
-            msg = MSG.from_address(int(message))
-            if msg.message == self.shellhook_msgid:
-                if msg.wParam == 1:
-                    self.created_windows.add(msg.hWnd)
-                    fire_shell_window_changed("created", msg.hWnd)
-                elif msg.wParam == 2:
-                    if msg.hWnd in self.created_windows:
-                        self.created_windows.remove(msg.hWnd)
-                    else:
-                        return
-                    fire_shell_window_changed("destroyed", msg.hWnd)
-                elif msg.wParam == 4:
-                    fire_shell_window_changed("activated", msg.hWnd)
-        return super().nativeEvent(eventType, message)
-
+    # def nativeEvent(self, eventType: QByteArray | bytes, message: int) -> object:
+    #     if eventType == "windows_generic_MSG":
+    #         msg = MSG.from_address(int(message))
+    #         if msg.message == self.shellhook_msgid:
+    #             if msg.wParam == 1:
+    #                 self.created_windows.add(msg.hWnd)
+    #                 fire_shell_window_changed("created", msg.hWnd)
+    #             elif msg.wParam == 2:
+    #                 if msg.hWnd in self.created_windows:
+    #                     self.created_windows.remove(msg.hWnd)
+    #                 else:
+    #                     return
+    #                 fire_shell_window_changed("destroyed", msg.hWnd)
+    #             elif msg.wParam == 4:
+    #                 fire_shell_window_changed("activated", msg.hWnd)
+    #     return super().nativeEvent(eventType, message)
 
 instance = WindowsSplash()
 
